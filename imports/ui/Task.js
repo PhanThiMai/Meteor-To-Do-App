9583b35fc9d60991ea7e20d13bc83f495ec107b6
Phan Thi Mai <1612369@student.hcmus.edu.vn>
2019-10-30 10:30:41 +0700

    implement hide private task

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Tasks } from '../api/tasks.js';
+import classnames from 'classnames';
 import { Meteor } from 'meteor/meteor';
 
 export default class Task extends Component {
@@ -14,11 +15,19 @@ export default class Task extends Component {
         Meteor.call('tasks.remove', this.props.task._id);
     }
 
+    togglePrivate = () => {
+        Meteor.call('tasks.setPrivate', this.props.task._id, !this.props.task.private);
+    }
+
+
 
     render() {
         // Give tasks a different className when they are checked off,
         // so that we can style them nicely in CSS
-        const taskClassName = this.props.task.checked ? 'checked' : '';
+        const taskClassName = classnames({
+            checked: this.props.task.checked,
+            private: this.props.task.private,
+        });
 
         return (
             <li className={taskClassName}>
@@ -31,6 +40,11 @@ export default class Task extends Component {
                     checked={!!this.props.task.checked}
                     onClick={this.toggleChecked}
                 />
+                {this.props.showPrivateButton ? (
+                    <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+                        {this.props.task.private ? 'Private' : 'Public'}
+                    </button>
+                ) : ''}
 
                 <span className="text">
                     <strong>{this.props.task.username}</strong>: {this.props.task.text}
